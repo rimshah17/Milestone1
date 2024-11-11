dfa39879513b6e101fdd95bb45e1791b043b516b
rimshah17 <rimshahali11@gmail.com>
2024-11-11 23:53:04 +0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Hello, World!</h1>
+        <p>My First Milestone in Nextjs</p>
+      </div>
+    );
+  }
